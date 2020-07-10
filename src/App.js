82043d626593585f122bb8fedc0c1b0d796a1f77
Amir Shariff <amirshariff@msn.com>
2020-07-10 03:25:19 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,52 +1,71 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/NavBar';
-import NavbarBrand from 'react-bootstrap/NavbarBrand';
-import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import './App.css';
+
+import Footer from './components/Footer';
+import HomePage from './Pages/HomePage';
+import ContactPage from './Pages/ContactPage';
+import AboutPage from './Pages/AboutPage';
 
 
-class App extends Component {
-	state = {
-		title: 'Amir Shariff',
-		headerLinks: [
-			{ title: 'Home', path: '/' },
-			{ title: 'About', path: 'about' },
-			{ title: 'Contact', path: 'contact' },
-		],
-		home: {
-			title: 'Be Relentless',
-			subtitle: 'Projects that make a diffrence',
-			text: 'Checkout my projects below',
-		},
-		about: {
-			title: 'About me',
-		},
-		contact: {
-			title: 'Contact me',
-		},
-	};
+
+class App extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			title: 'Amir Shariff',
+			headerLinks: [
+				{ title: 'Home', path: '/' },
+				{ title: 'About', path: '/about' },
+				{ title: 'Contact', path: '/contact' }
+			],
+			home: {
+				title: 'Be Relentless',
+				subTitle: 'Projects the make a difference',
+				text: 'Checkout my projects below'
+			},
+			about: {
+				title: 'About Me'
+			},
+			contact: {
+				title: 'Let\'s Talk'
+			}
+		}
+	}
+
 	render() {
 		return (
 			<Router>
 				<Container className="p-0" fluid={true}>
-					<Navbar className="border-bottom" bg='transparent' expand='lg'>
-						<NavbarBrand>Amir Shariff</NavbarBrand>
-						<Navbar.Toggle className='border-0' aria-controls="navbar-toggle" />
+
+					<Navbar className="border-bottom" bg="transparent" expand="lg">
+						<Navbar.Brand>Amir Shariff</Navbar.Brand>
+
+						<Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
 						<Navbar.Collapse id="navbar-toggle">
-							<Nav className='ml-auto'>
-								<Link className="nav-link" to="/">
-									Home
-								</Link>
-								<Link className="nav-link" to="/about">
-									About
-								</Link>
-								<Link className="nav-link" to="/contact">
-									Contact
-								</Link>
+							<Nav className="ml-auto">
+								<Link className="nav-link" to="/">Home</Link>
+								<Link className="nav-link" to="/about">About</Link>
+								<Link className="nav-link" to="/contact">Contact</Link>
 							</Nav>
 						</Navbar.Collapse>
 					</Navbar>
+					<Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
+					<Route path="/contact" render={() => <ContactPage title={this.state.contact.title} />} />
+					<Route path="/about" render={() => <AboutPage title={this.state.about.title} />} />
+
+
+
+
+
+
+
+					<Footer />
+
 				</Container>
 			</Router>
 		);
